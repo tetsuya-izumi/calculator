87ef8a672a62cfca5080cf02f26855ab79dcca17
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-02-14 23:05:29 +0900

    inputのcreateと非表示の実装途中です。

--- calculator/main.js
+++ calculator/main.js
@@ -1,8 +1,72 @@
-var number = document.getElementById('box')
-var calc_origin = number.value
+var plus = document.getElementById('plus_button')
+var minus = document.getElementById('minus_button')
+var multi = document.getElementById('multi_button')
+var divide = document.getElementById('divide_button')
 
-var button = document.getElementById('plus_button')
-button.addEventListener('click', function () {
-    calc_origin += 1;
-    console.log(calc_origin);
+///////////////////////////////////////////////// 足し算
+plus.addEventListener('click', function () {
+    var number1 = document.getElementById('num1')
+    var number2 = document.getElementById('num2')
+
+    // 入力された値を十進数の数値として扱う
+    var calc_origin1 = parseInt(number1.value, 10)
+    var calc_origin2 = parseInt(number2.value, 10)
+
+    // 数値1と数値2を計算
+    var plus_result = calc_origin1 + calc_origin2;
+
+    // 表示
+    console.log(plus_result);
+    document.getElementById('result').innerHTML = plus_result;
+});
+
+///////////////////////////////////////////////// 引き算
+minus.addEventListener('click', function () {
+    var number1 = document.getElementById('num1')
+    var number2 = document.getElementById('num2')
+
+    // 入力された値を十進数の数値として扱う
+    var calc_origin1 = parseInt(number1.value, 10)
+    var calc_origin2 = parseInt(number2.value, 10)
+
+    // 数値1と数値2を計算
+    var minus_result = calc_origin1 - calc_origin2;
+
+    // 表示
+    console.log(minus_result);
+    document.getElementById('result').innerHTML = minus_result;
+});
+
+///////////////////////////////////////////////// 掛け算
+multi.addEventListener('click', function () {
+    var number1 = document.getElementById('num1')
+    var number2 = document.getElementById('num2')
+
+    // 入力された値を十進数の数値として扱う
+    var calc_origin1 = parseInt(number1.value, 10)
+    var calc_origin2 = parseInt(number2.value, 10)
+
+    // 数値1と数値2を計算
+    var multi_result = calc_origin1 * calc_origin2;
+
+    // 表示
+    console.log(multi_result);
+    document.getElementById('result').innerHTML = multi_result;
+});
+
+///////////////////////////////////////////////// 割り算
+divide.addEventListener('click', function () {
+    var number1 = document.getElementById('num1')
+    var number2 = document.getElementById('num2')
+
+    // 入力された値を十進数の数値として扱う
+    var calc_origin1 = parseInt(number1.value, 10)
+    var calc_origin2 = parseInt(number2.value, 10)
+
+    // 数値1と数値2を計算
+    var divide_result = calc_origin1 / calc_origin2;
+
+    // 表示
+    console.log(divide_result);
+    document.getElementById('result').innerHTML = divide_result;
 });
